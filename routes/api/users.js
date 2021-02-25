7110ea01982873a5ccec24eb9cd00239519dbe36
sailakshmi-pattabiraman <sailakshmi_pattabiraman@apple.com>
2021-02-25 10:37:46 -0800

    Create a new user if not already registered

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/test", (req,res) => res.json({msg: "This is users route"}));
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
+
+// router.get("/test", (req,res) => res.json({msg: "This is users route"}));
+router.post("/register", (req, res) => {
+    User.findOne({email: req.body.email})
+        .then((user) => {
+            if(user){
+                //Throw status 400 err if the user is already registered
+                return res.status(400).json({email: "User has already registered"});
+            }else{
+                //Otherwise create a new user
+                const newUser =  new User({
+                    handle: req.body.handle,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                    })
+                });
+            }
+        })
+});
 
 module.exports = router;
