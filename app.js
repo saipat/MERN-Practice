1e8d83f6643d8fe026b951d58872c8b94a7bcc81
sailakshmi-pattabiraman <sailakshmi_pattabiraman@apple.com>
2021-02-25 05:59:46 -0800

    new file - users and tweets.

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+//to parse the JSON that we send to the FE
+const bodyParser = require('body-parser');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send('Hello World!!'));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json);
 
 
 //To deploy on Heroku || to run locally on port 5000
