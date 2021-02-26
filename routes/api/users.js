8b050a0ad632125c457322e216b3b4da2a691e15
sailakshmi-pattabiraman <sailakshmi_pattabiraman@apple.com>
2021-02-26 10:44:35 -0800

    Add bcrypt to login and registor functionalition.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
 
+//test route
 router.get("/test", (req,res) => res.json({msg: "This is users route"}));
+
+//new user registration
 router.post("/register", (req, res) => {
-    User.findOne({ email: req.body.email })
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+
+    User.findOne({ handle: req.body.handle })
         .then((user) => {
             if(user){
-                //Throw status 400 err if the user is already registered
-                return res.status(400).json({email: "User has already registered"});
+                errors.handle = "User already exists";
+                return res.status(400).json(errors);
             }else{
                 //Otherwise create a new user
                 const newUser =  new User({
@@ -22,17 +34,63 @@ router.post("/register", (req, res) => {
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
-                        newUser.password = hash;
-                        newUser.save()
-                        .then(user => res.json(user))
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => {
+                            const payload = { id: user.id, handle: user.handle };
+
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
                         .catch(err => console.log(err));
                     });
                 });
-
-                newUser.save().then(user => res.send(user)).catch(err =>res.send(err));
             }
         })
+});
+
+//login functionality
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
+    const handle = req.body.handle;
+    const password = req.body.password;
+
+    User.findOne({ handle })
+        .then(user => {
+            if(!user){
+                errors.handle = "This user does not exist";
+                return res.status(400).json(errors);
+            }
+            
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        const payload = { id: user.id, handle: user.handle };
+
+                        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                        });
+                    } else {
+                        errors.password = "Incorrect password";
+                        return res.status(400).json(errors);
+                    }
+                });
+
+        });
 });
 
 module.exports = router;
